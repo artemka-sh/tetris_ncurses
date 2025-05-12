9f203f73f70af1cae3d9a550f7f6e099f6604801
artemka-sh <artemmmchik@mail.ru>
2025-05-12 23:14:57 +0300

    работает_я_и_рад

---
+++ test/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/Artem/vscode/tetris_ncurses/test
+# Build directory: /home/Artem/vscode/tetris_ncurses/test/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(TetrisTest "/home/Artem/vscode/tetris_ncurses/test/build/tetris_tests")
+set_tests_properties(TetrisTest PROPERTIES  _BACKTRACE_TRIPLES "/home/Artem/vscode/tetris_ncurses/test/CMakeLists.txt;25;add_test;/home/Artem/vscode/tetris_ncurses/test/CMakeLists.txt;0;")
